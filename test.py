cdafc03fca91c6b3a535c0569430e2615e36ad70
Chowdary, Subhadra <subhadra.chowdary@ge.com>
2020-01-29 17:51:37 -0500

    'updated'

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ if response.startswith('n'):
     message = input("What message you want?\n")
 
 cp = cmd.run(f"git commit -a -m '{message}'", check=True, shell=True)
-cp = cmd.run("git remote add origin git@github.com:subhadra-chinnu/CodeDeployGitHubDemo.git", check=True, shell=True)
+#cp = cmd.run("git remote add origin git@github.com:subhadra-chinnu/CodeDeployGitHubDemo.git", check=True, shell=True)
 cp = cmd.run("git push -u origin master -f", check=True, shell=True)
 
 
