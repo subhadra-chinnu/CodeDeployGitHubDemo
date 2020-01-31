ec6e631010317b4cf59f24154792c99ac89922a0
subhadra <47237476+subhadra-chinnu@users.noreply.github.com>
2020-01-31 13:46:51 -0500

    Update sample.py

--- test/sample.py
+++ test/sample.py
@@ -1,7 +1,7 @@
 import subprocess as cmd
 
 cp = cmd.run("git add .", check=True, shell=True)
-print(cp)
+#print(cp)
 
 response = input("Do you want to use the default message for this commit?([y]/n)\n")
 message = "update the repository"
@@ -9,8 +9,7 @@ message = "update the repository"
 if response.startswith('n'):
     message = input("What message you want?\n")
 
-cp = cmd.run(f"git commit -a -m '{message}'", check=True, shell=True)
-#cp = cmd.run("git remote add origin git@github.com:subhadra-chinnu/CodeDeployGitHubDemo.git", check=True, shell=True)
+cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
 cp = cmd.run("git push -u origin master -f", check=True, shell=True)
 
 
